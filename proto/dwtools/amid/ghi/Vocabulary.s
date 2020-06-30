28137c865af929f516a38fe30861b9217bf3200d
wandalen <wandalen.me@gmail.com>
2020-06-30 09:12:13 +0300

    .

--- proto/dwtools/amid/ghi/Vocabulary.s
+++ proto/dwtools/amid/ghi/Vocabulary.s
@@ -5,22 +5,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  if( typeof _global_ === 'undefined' || !Object.hasOwnProperty.call( _global_, 'wBase' ) )
-  {
-    let toolsPath = '../../../dwtools/Base.s';
-    let toolsExternal = 0;
-    try
-    {
-      toolsPath = require.resolve( toolsPath );
-    }
-    catch( err )
-    {
-      toolsExternal = 1;
-      require( 'wTools' );
-    }
-    if( !toolsExternal )
-    require( toolsPath );
-  }
+  require( '../../../dwtools/Tools.s' );
 
   var _ = _global_.wTools;
 
